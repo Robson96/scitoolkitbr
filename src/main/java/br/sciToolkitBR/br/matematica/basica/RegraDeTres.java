2ce692565060df40bb6c5597ae7d6806d3b60045
robson <53229150+Robson96@users.noreply.github.com>
2025-07-12 21:37:24 -0300

    Update RegraDeTres.java

--- src/main/java/br/sciToolkitBR/br/matematica/basica/RegraDeTres.java
+++ src/main/java/br/sciToolkitBR/br/matematica/basica/RegraDeTres.java
@@ -4,15 +4,15 @@ import org.apache.commons.math3.util.Precision;
 
 public class RegraDeTres {
     /**
-     * Calcula o valor de uma regra de três simples:
-     * Se A está para B, então C está para X.
+     * Calcula o valor de uma regra de tres simples:
+     * Se A está para B, então C esta para X.
      *
      * Fórmula: X = (B * C) / A
      *
      * @param a valor conhecido
      * @param b valor conhecido
      * @param c valor conhecido
-     * @return resultado da regra de três simples
+     * @return resultado da regra de tres simples
      * @throws IllegalArgumentException se 'a' for zero
      */
     public static double calcular(double a, double b, double c) {
@@ -21,28 +21,6 @@ public class RegraDeTres {
         }
 
         var resultado = (b * c) / a;
-
-        return Precision.round(resultado, 2);
-    }
-
-    /**
-     * Calcula o valor de uma regra de três simples:
-     * Se A está para B, então C está para X.
-     *
-     * Fórmula: X = (A * B) / C
-     *
-     * @param a valor conhecido
-     * @param b valor conhecido
-     * @param c valor conhecido
-     * @return resultado da regra de três inversamente proporcional
-     * @throws IllegalArgumentException se 'c' for zero
-     */
-    public static double calcularInversamente(double a, double b, double c) {
-        if (c == 0) {
-            throw new IllegalArgumentException("O valor de 'c' não pode ser zero");
-        }
-
-        var resultado = (a * b) / c;
 
         return Precision.round(resultado, 2);
     }
